30dda22277b23df21a69829bdd5c398f13a82748
Eikcaj.1998 <jw220998@gmail.com>
2022-09-19 17:03:11 +0200

    esercizio finito

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
-    //
+    public function index()
+     {
+         $movies = Movie::all();
+         return view('home', compact('movies'));
+     }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+
+@extends('layout.main')
+
+@section('title', 'Home')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex flex-wrap">
+                @forelse ($movies as $movie)
+                    <div class="card m-5" style="width: 18rem;">
+                        <div class="card-body">
+                            <h2>{{ $movie->title }}</h2>
+                            <h4>Original Title:{{ $movie->original_title }}</h4>
+                            <h5>Nationality:{{ $movie->nationality }}</h5>
+                            <h6>Release date:{{ $movie->date }}</h6>
+                            <h6> Vote:{{ $movie->vote }}</h6>
+                        </div>
+                    </div>
+                @empty
+                @endforelse
+            </div>
+        </div>
+    </div>
+
+@endsection
